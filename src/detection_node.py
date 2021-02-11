6736e911b8a0f2e76f7990c7fa9ae36ae2581b77
Yegor Alexandrow <yegor99@live.com>
2021-02-11 16:15:04 +0300

    initial commit

---
+++ src/detection_node.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+if __name__ == 'main':
+    rospy.init_node('detection_node')
+    rospy.spin()
---
+++ src/segmentation_node.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+
+if __name__ == 'main':
+    rospy.init_node('segmentation_node')
+    rospy.spin()
